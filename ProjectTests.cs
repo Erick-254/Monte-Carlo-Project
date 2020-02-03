0419db2ca6d4faf4bbc28c20ac70c8a03caa9981
erotich <58568904+erotich@users.noreply.github.com>
2020-02-03 03:53:24 +0300

    Add files via upload

---
+++ ProjectTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonteCarlo
+{
+    class ProjectTests
+    {
+   
+       public void TestBucket()
+       {
+          Operation operations = new Operation();
+          int[] Estimation = operations.CalculateEstimated();
+            int min = Estimation[0];
+            int max = Estimation[2];
+            var bucket = new Bucket(10, 40, 160);
+ 
+       }
+
+    }
+}
